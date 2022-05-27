db1179da36defb19d6045a33946f8f54d9f31b04
servon <servonlewis@gmail.com>
2022-05-27 11:37:08 -0400

    Initial Commit

---
+++ Ui Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ui Design
+//
+//  Created by Servon Lewis on 5/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ui Design/Ui_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ui_DesignApp.swift
+//  Ui Design
+//
+//  Created by Servon Lewis on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Ui_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
